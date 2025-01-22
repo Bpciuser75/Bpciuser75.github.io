0162d80de31a6ed7362cd47047804ade24539b05
Bpciuser75 <elizabeth.ashiru@student.tdsb.on.ca>
2025-01-22 13:57:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -121,19 +121,19 @@ function checkPose(prediction, video) {
 
         switch(poseNumber) {
             case '1':
-                if (time >= 0.9 && time <= 3.0 && !poseState.triggered) {
+                if (time >= 1.0 && time <= 3.0 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '2':
-                if (time >= 5.5 && time <= 7.5 && !poseState.triggered) {
+                if (time >= 5.0 && time <= 7.0 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '3':
-                if ((time >= 11.5 && time <= 13.0 && !poseState.firstWindowTriggered) ||
-                    (time >= 17.5 && time <= 19.5 && !poseState.secondWindowTriggered)) {
-                    if (time <= 13.0) {
+                if ((time >= 8.0 && time <= 9.0 && !poseState.firstWindowTriggered) ||
+                    (time >= 9.0 && time <= 10.0 && !poseState.secondWindowTriggered)) {
+                    if (time <= 9.0) {
                         poseState.firstWindowTriggered = true;
                     } else {
                         poseState.secondWindowTriggered = true;
@@ -144,12 +144,12 @@ function checkPose(prediction, video) {
                 }
                 break;
             case '4':
-                if (time >= 15.5 && time <= 16.6 && !poseState.triggered) {
+                if (time >= 11.0 && time <= 13.0 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '5':
-                if (time >= 19.5 && !poseState.triggered) {
+                if (time >= 15.0 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
