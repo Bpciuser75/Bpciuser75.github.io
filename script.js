1f43a95f7f50d9b9dad38eeb095d859c268df32e
Bpciuser75 <elizabeth.ashiru@student.tdsb.on.ca>
2025-01-22 13:27:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/CMBhs4EAW/";
+let URL = "https://teachablemachine.withgoogle.com/models/rHp1OWhyj/";
 
-https://teachablemachine.withgoogle.com/models/[...]
+
 
 
 
@@ -234,7 +234,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/CMBhs4EAW/");
+        console.log("https://teachablemachine.withgoogle.com/models/rHp1OWhyj/");
     }
 }
 
